8708766b04f52831652062656f522e801ed97a2f
Marc S. Brooks <marc@mbrooks.info>
2013-02-12 00:55:02 -0800

    Added script to fetch/install SSH pubkey from instance metadata

---
+++ ec2/get-pubkey.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+#  get-pubkey.sh
+#  Install the public key for the select user
+#
+#  Copyright 2008-2013, Marc S. Brooks (http://mbrooks.info)
+#  Licensed under the MIT license:
+#  http://www.opensource.org/licenses/mit-license.php
+#
+#  Dependencies:
+#    ec2-metadata
+#
+#  Notes:
+#   - This script has been tested to work with RHEL & CentOS
+#   - This script must be run as root
+#   - This script can be installed in /etc/init.d or run from the command-line
+#   - The authorized_keys outfile requires local user permissions
+#
+#  chkconfig: 3 97 97
+#  description: Account update script
+#
+
+. /etc/init.d/functions
+
+OUTFILE=/path/to/user/.ssh/authorized_keys
+
+if [ -f $OUTFILE ]; then
+    action $"Public key installed:" `/usr/local/bin/ec2-metadata -u | tail -1 > $OUTFILE`
+else
+    exit 1
+fi
+
+exit 0
